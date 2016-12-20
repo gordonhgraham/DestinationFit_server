e2f40bdb9e327217e1ed2cd588e95a72a3da498b
Gordon H Graham <theboss@gordonhgraham.com>
2016-12-20 15:09:15 -0700

    lint generated files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+'use strict'
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const express = require(`express`)
+const router = express.Router()
 
-module.exports = router;
+/* GET home page */
+router.get(`/`, (req, res, next) => {
+})
+
+module.exports = router
